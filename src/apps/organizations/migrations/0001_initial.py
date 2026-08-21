28b08db8e56654da85da98a2409dfb22b8547ccb
Gerardo Rodney Ticona Moscoso <gerardo.ticona.m7@gmail.com>
2026-08-20 19:42:41 -0500

    fix(p07): alinear índices de migración

--- src/apps/organizations/migrations/0001_initial.py
+++ src/apps/organizations/migrations/0001_initial.py
@@ -260,7 +260,7 @@ class Migration(migrations.Migration):
                 "indexes": [
                     models.Index(
                         fields=["site", "is_active", "code"],
-                        name="organizations_service_catalog_ix",
+                        name="org_service_catalog_ix",
                     )
                 ],
                 "constraints": [
@@ -365,7 +365,7 @@ class Migration(migrations.Migration):
                 "indexes": [
                     models.Index(
                         fields=["organization", "parent"],
-                        name="organizations_area_hierarchy_ix",
+                        name="org_area_hierarchy_ix",
                     )
                 ],
                 "constraints": [
